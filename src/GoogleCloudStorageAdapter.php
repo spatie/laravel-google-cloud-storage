1484bfa6aaa55889ba565a9e5130804cffb55aa4
Markus Machatschek <markus.machatschek@hey.com>
2022-01-24 20:40:09 +0100

    wip

---
+++ src/GoogleCloudStorageAdapter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Spatie\GoogleCloudStorage;
+
+use Google\Cloud\Storage\Bucket;
+use Google\Cloud\Storage\Connection\Rest;
+use League\Flysystem\GoogleCloudStorage\GoogleCloudStorageAdapter as BaseAdapter;
+use League\Flysystem\GoogleCloudStorage\VisibilityHandler;
+use League\Flysystem\Visibility;
+
+class GoogleCloudStorageAdapter extends BaseAdapter
+{
+    protected string $storageApiUrl;
+    public function __construct(
+        Bucket $bucket,
+        string $prefix = '',
+        VisibilityHandler $visibilityHandler = null,
+        string $defaultVisibility = Visibility::PRIVATE,
+        ?string $storageApiUrl = null,
+    ) {
+        parent::__construct($bucket, $prefix, $visibilityHandler, $defaultVisibility);
+
+        $this->storageApiUrl = $storageApiUrl ?? Rest::DEFAULT_API_ENDPOINT;
+    }
+}
--- src/GoogleCloudStorageServiceProvider.php
+++ src/GoogleCloudStorageServiceProvider.php
@@ -3,67 +3,54 @@
 namespace Spatie\GoogleCloudStorage;
 
 use Google\Cloud\Storage\StorageClient;
-use Illuminate\Filesystem\Cache;
-use Illuminate\Filesystem\FilesystemManager;
+use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
-use League\Flysystem\AdapterInterface;
-use League\Flysystem\Cached\CachedAdapter;
-use League\Flysystem\Cached\CacheInterface;
-use League\Flysystem\Cached\Storage\Memory as MemoryStore;
 use League\Flysystem\Filesystem;
-use Spatie\GoogleCloudStorageAdapter\GoogleCloudStorageAdapter;
+use League\Flysystem\Visibility;
 
 class GoogleCloudStorageServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $factory = $this->app->make('filesystem');
-
-        /* @var FilesystemManager $factory */
-        $factory->extend('gcs', function ($_app, $config) {
-            $adapter = $this->createAdapter($config);
-
-            return $this->createFilesystem($adapter, $config);
+        Storage::extend('gcs', function ($app, $config) {
+            return $this->createFilesystemAdapter($config);
         });
+    }
+
+    protected function createFilesystemAdapter(array $config): FilesystemAdapter
+    {
+        $config = Arr::only($config, [
+            'bucket',
+            'path_prefix',
+            'visibility',
+            'disable_asserts',
+            'storage_api_uri',
+            'metadata'
+        ]);
+        $adapter = $this->createAdapter($config);
+
+        return new FilesystemAdapter(new Filesystem($adapter, $config), $adapter, $config);
     }
 
     protected function createAdapter(array $config): GoogleCloudStorageAdapter
     {
         $storageClient = $this->createClient($config);
-
-        $bucket = $storageClient->bucket($config['bucket']);
-
+        $bucket = $storageClient->bucket(Arr::get($config, 'bucket'));
         $pathPrefix = Arr::get($config, 'path_prefix');
+        $visibility = Arr::get($config, 'visibility');
+        $defaultVisibility = in_array(
+            $visibility,
+            [
+                Visibility::PRIVATE,
+                Visibility::PUBLIC,
+            ]
+        ) ? $visibility : Visibility::PRIVATE;
+
         $storageApiUri = Arr::get($config, 'storage_api_uri');
 
-        return new GoogleCloudStorageAdapter($storageClient, $bucket, $pathPrefix, $storageApiUri);
-    }
-
-    protected function createFilesystem(AdapterInterface $adapter, array $config): Filesystem
-    {
-        $cache = Arr::pull($config, 'cache');
-
-        $config = Arr::only($config, ['visibility', 'disable_asserts', 'url', 'metadata']);
-
-        if ($cache) {
-            $adapter = new CachedAdapter($adapter, $this->createCacheStore($cache));
-        }
-
-        return new Filesystem($adapter, count($config) > 0 ? $config : null);
-    }
-
-    protected function createCacheStore(array | bool $config): CacheInterface
-    {
-        if ($config === true) {
-            return new MemoryStore();
-        }
-
-        return new Cache(
-            $this->app->get('cache')->store($config['store']),
-            Arr::get($config, 'prefix', 'flysystem'),
-            Arr::get($config, 'expire')
-        );
+        return new GoogleCloudStorageAdapter($bucket, $pathPrefix, null, $defaultVisibility, $storageApiUri);
     }
 
     protected function createClient(array $config): StorageClient
